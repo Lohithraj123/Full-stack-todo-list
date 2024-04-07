9500725e674e0ee18f1692ee689fbbabc1651580
mlohithraj <rlohith767@gmail.com>
2024-04-07 21:56:07 +0530

    Updating status

--- components/todo/TodoList.js
+++ components/todo/TodoList.js
@@ -1,33 +1,53 @@
 import React from 'react';
+import { MongoClient } from 'mongodb';
 import { FaEdit } from 'react-icons/fa';
 import { IoMdDoneAll } from 'react-icons/io';
 import classes from './TodoList.module.css';
 import Link from 'next/link';
 import TodoDelete from './TodoDelete';
 
-const TodoList = () => {
+const TodoList = (props) => {
   const DUMMY_DATA = [
     {
       todo: 'Finish the sharpener Task',
     },
   ];
 
-  return (
-    <div className={classes.items}>
-      {DUMMY_DATA.map((item, index) => (
-        <div key={index} className={classes.mappedItems}>
-          <p>{item.todo}</p>
-          <Link href='todo-completed'>
-            <IoMdDoneAll />
-          </Link>
-          <TodoDelete />
-          <Link href="edit/id">
-            <FaEdit />
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
+  const getStaticProps = async () => {
+    const client = await MongoClient.connect(
+      'mongodb+srv://mlohithraj5:nJWeEdXDix5J4O96@cluster0.vkj3gpn.mongodb.net/todo?retryWrites=true&w=majority&appName=Cluster0',
+    );
+    const db = client.db();
+
+    const todoCollection = db.collection('todo');
+
+    const todoList = await todoCollection.find().toArray();
+  
+
+  return {
+    // <div className={classes.items}>
+    //   {DUMMY_DATA.map((item, index) => (
+    //     <div key={index} className={classes.mappedItems}>
+    //       <p>{item.todo}</p>
+    //       <Link href="todo-completed">
+    //         <IoMdDoneAll />
+    //       </Link>
+    //       <TodoDelete />
+    //       <Link href="edit/id">
+    //         <FaEdit />
+    //       </Link>
+    //     </div>
+    //   ))}
+    // </div>
+    
+      props: {
+        todoList: todoList.map(todo => ({
+          title: todo.title
+        }))
+      },
+      revalidate: 1,
+    }
+  }
 };
 
 export default TodoList;
--- pages/api/route-get.js
+++ pages/api/route-get.js
@@ -3,22 +3,20 @@ import { MongoClient } from 'mongodb';
 import React from 'react';
 
 const handler = async (req, res) => {
-  if (req.method === 'POST') {
-    const data = req.body;
-
+  if (req.method === 'GET') {
     const client = await MongoClient.connect(
       'mongodb+srv://mlohithraj5:nJWeEdXDix5J4O96@cluster0.vkj3gpn.mongodb.net/todo?retryWrites=true&w=majority&appName=Cluster0',
     );
     const db = client.db();
 
-    const meetupsCollection = db.collection('todo');
-    const result = await meetupsCollection.insertOne({ data });
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.find().toArray();
 
     console.log(result);
 
     client.close();
 
-    res.status(201).json({ message: 'Succesfully added' });
+    res.status(201).json({ message: { result } });
   }
 };
 

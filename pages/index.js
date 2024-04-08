7f572706b9e30090d2f40034c90c11544454ac52
mlohithraj <rlohith767@gmail.com>
2024-04-08 23:34:44 +0530

    Cimitting changes

--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,13 @@ import MeetupList from '@/components/todo/TodoList';
 import { MongoClient } from 'mongodb';
 import Head from 'next/head';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://tse1.mm.bing.net/th?id=OIP.YUI2W_3AYOxsG_STV-PJpAHaFj&pid=Api&P=0&h=180',
-    address: 'Some Address 1',
-    description: 'THis is the first meet up',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://tse1.mm.bing.net/th?id=OIP.YUI2W_3AYOxsG_STV-PJpAHaFj&pid=Api&P=0&h=180',
-    address: 'Some Address 2',
-    description: 'THis is the second meet up',
-  },
-];
 
 export default function Home(props) {
   return (
     <>
       <Head>
-        <title>Meet Up Page</title>
-        <meta name="description" content="A new meetup page app" />
+        <title>Todo Page</title>
+        <meta name="description" content="A new Todo page app" />
       </Head>
       <MeetupList meetups={props.meetups} />
     </>
